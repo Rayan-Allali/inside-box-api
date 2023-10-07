02d72bebcb039ee9cc61b359251c28033b502bac
Rayan-Allali <108684980+Rayan-Allali@users.noreply.github.com>
2023-10-07 04:08:10 +0100

    fixing auth

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { config } from 'dotenv';
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', 
+    methods: '*',
+    allowedHeaders: '*',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
